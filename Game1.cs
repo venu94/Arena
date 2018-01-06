a53ab8c8a4edbb06d54879a7c5f3d7cce5a16e2c
Shehan <mithwick93@gmail.com>
2018-01-06 19:29:28 +0530

    draw obstacles

--- Game1.cs
+++ Game1.cs
@@ -84,7 +84,8 @@ namespace arena_trail
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
@@ -103,7 +104,7 @@ namespace arena_trail
                 GraphicsDevice.Clear(Color.Black);
                 spriteBatch.Begin();
                 DrawBackground();
-                DrawStaticObstacle();
+                DrawStaticObstacles();
                 // DrawDynamicObstacle();
                 //DrawRobot();
 
@@ -127,75 +128,94 @@ namespace arena_trail
 
         private void DrawStaticObstacles()
         {
-            // Rectangle screenRectangle = new Rectangle(10, 40, Setting.TileSize, Setting.TileSize);
-            //  spriteBatch.Draw(boxTexture, screenRectangle, Color.White);
+            // Dummy Two-dimensional array.
+            int[,] array2D = new int[,] {{1, 1}, {2, 1}, {3, 1}, {4, 1}, {2,1}, { 2, 2 }, { 2, 3 } };
 
-            for (var i = 0; i < 50; i++)
-            { 
-            
-            }
-
-             private void DrawStaticObstacle()
-        {
-            Rectangle screenRectangle = new Rectangle(10, 40, Setting.TileSize, Setting.TileSize);
-              spriteBatch.Draw(boxTexture, screenRectangle, Color.White);
-
-
-                 //int x, int y 
-            
-            /*
-            private void DrawGridLines()
+            for (var i = 0; i < 7; i++)
             {
-                Rectangle screenRectangle = new Rectangle(0, 0, Setting.ReslutionWidth, Setting.ReslutionHeight);
-                spriteBatch.Draw(gridMatrix, screenRectangle, Color.White);
+                DrawStaticObstacle(array2D[i, 0], array2D[i, 1]);
             }
-
-            private Texture2D GenerateGrid(Rectangle destRect, int cols, int rows, Color gridColor)
-            {
-                RenderTarget2D grid = new RenderTarget2D(GraphicsDevice, destRect.Width,
-                    destRect.Height);
-                try
-                {
-                    float gridCellWidth = destRect.Width / (float)cols;
-                    float gridCellHight = destRect.Height / (float)rows;
-                    int w = (int)(cols * gridCellWidth);
-                    int h = (int)(rows * gridCellHight);
-
-                    float uselessWidth = destRect.Width - w;
-                    float uselessHight = destRect.Height - h;
-
-                    Rectangle bounds = new Rectangle((int)(uselessWidth / 2) + destRect.X, (int)(uselessHight / 2) + destRect.Y, w, h);
-
-                    GraphicsDevice.SetRenderTarget(grid);
-                    GraphicsDevice.Clear(Color.Transparent);
-
-                    primitiveBatch.Begin(PrimitiveType.LineList);
-
-                    float x = bounds.X;
-                    float y = bounds.Y;
-
-                    for (int col = 0; col < cols + 1; col++)
-                    {
-                        primitiveBatch.AddVertex(new Vector2(x + (col * gridCellWidth), bounds.Top), gridColor);
-                        primitiveBatch.AddVertex(new Vector2(x + (col * gridCellWidth), bounds.Bottom), gridColor);
-                    }
-
-                    for (int row = 0; row < rows + 1; row++)
-                    {
-                        primitiveBatch.AddVertex(new Vector2(bounds.Left, y + (row * gridCellHight)), gridColor);
-                        primitiveBatch.AddVertex(new Vector2(bounds.Right, y + (row * gridCellHight)), gridColor);
-                    }
-                    primitiveBatch.End();
-
-                    GraphicsDevice.SetRenderTarget(null);
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("GenerateGrid() Exception : " + ex.Message);
-                }
-                return grid;
-            }*/
         }
+
+        private void DrawStaticObstacle(int x, int y)
+        {
+            Rectangle screenRectangle = new Rectangle(x * Setting.TileSize, y * Setting.TileSize, Setting.TileSize,
+                Setting.TileSize);
+            spriteBatch.Draw(boxTexture, screenRectangle, Color.White);
+        }
+
+        //        private void DrawStaticObstacles()
+        //        {
+        //            // Rectangle screenRectangle = new Rectangle(10, 40, Setting.TileSize, Setting.TileSize);
+        //            //  spriteBatch.Draw(boxTexture, screenRectangle, Color.White);
+        //
+        //            for (var i = 0; i < 50; i++)
+        //            { 
+        //            
+        //            }
+        //
+        ////             private void DrawStaticObstacle()
+        //        {
+        //            Rectangle screenRectangle = new Rectangle(10, 40, Setting.TileSize, Setting.TileSize);
+        //              spriteBatch.Draw(boxTexture, screenRectangle, Color.White);
+        //
+        //
+        //                 //int x, int y 
+        //            
+        //            /*
+        //            private void DrawGridLines()
+        //            {
+        //                Rectangle screenRectangle = new Rectangle(0, 0, Setting.ReslutionWidth, Setting.ReslutionHeight);
+        //                spriteBatch.Draw(gridMatrix, screenRectangle, Color.White);
+        //            }
+        //
+        //            private Texture2D GenerateGrid(Rectangle destRect, int cols, int rows, Color gridColor)
+        //            {
+        //                RenderTarget2D grid = new RenderTarget2D(GraphicsDevice, destRect.Width,
+        //                    destRect.Height);
+        //                try
+        //                {
+        //                    float gridCellWidth = destRect.Width / (float)cols;
+        //                    float gridCellHight = destRect.Height / (float)rows;
+        //                    int w = (int)(cols * gridCellWidth);
+        //                    int h = (int)(rows * gridCellHight);
+        //
+        //                    float uselessWidth = destRect.Width - w;
+        //                    float uselessHight = destRect.Height - h;
+        //
+        //                    Rectangle bounds = new Rectangle((int)(uselessWidth / 2) + destRect.X, (int)(uselessHight / 2) + destRect.Y, w, h);
+        //
+        //                    GraphicsDevice.SetRenderTarget(grid);
+        //                    GraphicsDevice.Clear(Color.Transparent);
+        //
+        //                    primitiveBatch.Begin(PrimitiveType.LineList);
+        //
+        //                    float x = bounds.X;
+        //                    float y = bounds.Y;
+        //
+        //                    for (int col = 0; col < cols + 1; col++)
+        //                    {
+        //                        primitiveBatch.AddVertex(new Vector2(x + (col * gridCellWidth), bounds.Top), gridColor);
+        //                        primitiveBatch.AddVertex(new Vector2(x + (col * gridCellWidth), bounds.Bottom), gridColor);
+        //                    }
+        //
+        //                    for (int row = 0; row < rows + 1; row++)
+        //                    {
+        //                        primitiveBatch.AddVertex(new Vector2(bounds.Left, y + (row * gridCellHight)), gridColor);
+        //                        primitiveBatch.AddVertex(new Vector2(bounds.Right, y + (row * gridCellHight)), gridColor);
+        //                    }
+        //                    primitiveBatch.End();
+        //
+        //                    GraphicsDevice.SetRenderTarget(null);
+        //
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    Console.WriteLine("GenerateGrid() Exception : " + ex.Message);
+        //                }
+        //                return grid;
+        //            }*/
+        //        }
+        //    }
     }
 }
